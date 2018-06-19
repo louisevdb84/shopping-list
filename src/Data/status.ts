c616f56e37dc7a3ab2ecfc7590dd93f9850356a6
Louise van den Berg <louisevdb84@gmail.com>
2018-06-19 15:55:06 +0200

    add edit functionality to shops, and started adding it to items

--- src/Data/status.ts
+++
@@ -1,8 +0,0 @@
-import { Status } from '../Models/status.model';
-
-export let status: Status[] = [
-    // new Status("Permanent"),    
-    // new Status("Copy"),    
-    // new Status("Current"),    
-    // new Status("Done"),        
-];
--- src/Data/users.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../Models/user.model';
-
-export let users: User[] = [
-    // new User("louisevdb", "123"),
-    // new User("karenS", "456")
-];
-
