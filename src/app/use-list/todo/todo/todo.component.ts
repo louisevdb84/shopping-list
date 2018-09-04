2ca6a09b76dd06ee90efe83333402fb4e699f69b
Louise van den Berg <louisevdb84@gmail.com>
2018-09-04 13:05:38 +0200

    Previous stuff done

--- src/app/use-list/todo/todo/todo.component.ts
+++ src/app/use-list/todo/todo/todo.component.ts
@@ -1,35 +1,43 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Item } from '../../../../Models/item.model';
 import { ItemsService } from '../../../Services/items.service';
 import { ShopsService } from '../../../services/shops.service';
 import { Shop } from '../../../../Models/shop.model';
 import { Status } from '../../../../Models/status.model';
 import { StatusService } from '../../../services/status.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent implements OnInit, OnDestroy {
   @ViewChild('shopInput') shopInputRef: ElementRef;
+  @ViewChild('sorting') sortingInputRef: ElementRef;
+
+  subscription: Subscription;
+  subscription2: Subscription;
 
   items: Item[];
   shops: Shop[];
   status: Status[];
   selectedShop: Shop;
+  displayEdit = false;
+  isVisible = "hidden";
 
   constructor(
     private itemsService: ItemsService,
     private shopsService: ShopsService,
     private statusService: StatusService) { }
 
-  ngOnInit() {        
+  ngOnInit() {       
+    
     this.shopsService.getShops()
     .subscribe(
       (res) => {
         this.shops = res.json()
-        this.selectedShop = this.shops[0];
+        this.selectedShop = null;
       },
       (err) => console.log(err));
     
@@ -38,14 +46,39 @@ export class TodoComponent implements OnInit {
         (res) => this.status = res.json(),
         (err) => console.log(err));
     
+      this.subscription = this.itemsService.itemsChanged
+      .subscribe(
+        (res) => this.getItems("Current")
+      );
+    
+      this.subscription2 = this.itemsService.doneEditingAdding
+      .subscribe(
+        () => {
+          this.isVisible = "hidden";
+          this.displayEdit = false;
+        }
+      );
+    
     this.getItems("Current");    
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.subscription2.unsubscribe();
   }
 
   getItems(status) {    
     this.itemsService.getItemsByStatus('items', status)
       .subscribe(
-      (res) => {               
-        this.items = res.json().filter(item => item.shop.name === this.selectedShop.name)
+      (res) => {         
+        if (!this.selectedShop)
+        {
+          this.items = res.json();
+        }
+        else {
+          this.items = res.json().filter(item => item.shop.name === this.selectedShop.name)  
+        }
+        
       },        
       (err) => console.log(err));    
   }
@@ -55,6 +88,15 @@ export class TodoComponent implements OnInit {
     this.getItems("Current");    
   }
 
+  onSortingChange(event, item) {    
+    item.sorting = event.target.value;
+    this.itemsService.editItem(item)
+      .subscribe((res => {
+        this.getItems("Current")
+        this.itemsService.itemsChanged.next();
+      }));      
+  }
+
   onDone(item: Item) {
     item.status = this.status.find(s => s.name === "Done");
     this.itemsService.editItem(item)
@@ -62,5 +104,17 @@ export class TodoComponent implements OnInit {
         this.getItems("Current")
         this.itemsService.itemsChanged.next();
       }));        
+  }
+
+  onEdit(item: Item) {    
+    this.displayEdit = true;
+    this.isVisible = "visible"
+    this.itemsService.startedEditing.next(item);  
+    this.itemsService.itemsChanged.next();
+  }
+
+  onHide() {
+    this.isVisible = "hidden";
+    this.displayEdit = false;
   }
 }
